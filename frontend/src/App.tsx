c202c0ddce897179af458d4c4d5ba47931697da9
jbrard <Joshuaeli1234@gmail.com>
2021-07-12 21:03:30 -0400

    Testing: tree

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,54 @@
-import React, { Component } from 'react'; // let's also import Component
-export class App extends Component {
+import React from 'react';
+import './App.css';
+import Tree from 'react-d3-tree';
 
-  // The tick function sets the current state. TypeScript will let us know
-  // which ones we are allowed to set.
+export default class App extends React.Component {
 
-  // After the component did mount, we set the state each second.
-  componentDidMount() {
-    
-  }
-
-  // render will know everything!
   render() {
-    return <p> "Hello" </p>
+    let data = {
+      "name": "Electronic",
+      "children": [
+        {
+          "name": "DnB",
+          "children": [
+            {
+              "name": "Deep",
+              "children": []
+            },
+            {
+              "name": "Jungle",
+              "children": []
+            }
+          ]
+        },
+        {
+          "name": "House",
+          "children": [
+            {
+              "name": "Deep",
+              "children": []
+            },
+            {
+              "name": "BigRoom",
+              "children": []
+            }
+          ]
+        },
+        {
+          "name": "Dubstep",
+          "children": []
+        }
+      ]
+    }
+    return (
+    <h1> "Genre Tree"
+      <div>
+        <Tree
+          data={data}
+          orientation={'vertical'}
+        />
+      </div>
+    </h1>
+    )
   }
 }
-
-export default App
