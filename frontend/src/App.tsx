da796e7548ca964144b73fa19f09bec484d64e27
jbrard <Joshuaeli1234@gmail.com>
2021-07-15 21:59:31 -0400

    Testing: tree

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,45 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-//import './App.css';
+import Tree from 'react-d3-tree'
+import './custom-tree.css';
+import './App.css';
 
 interface IAppProps {
 }
 
+interface RawNodeDatum {
+  name: string;
+  attributes?: Record<string, string | number | boolean>;
+  children?: RawNodeDatum[];
+}
+
 const App: React.FC<IAppProps> = props => {
 
-  const [ name, setName ] = useState<string>('');
+  const [tree, setTree] = useState<RawNodeDatum>({name:'init'});
 
-  const getName = async () => {
-    await fetch("/api/hello")
-    .then(response => response.json())
-    .then(responseJson => {
-      setName(responseJson.data)
-    })
+  const getTree = async () => {
+    await fetch("/api/tree")
+      .then(response => response.json())
+      .then(responseJson => {
+        setTree(responseJson.data)
+      })
   }
 
   useEffect(() => {
-    getName();
+    getTree();
   }, []);
 
   return (
-    <main className="container">
-      <h1> {name} </h1>
-    </main>
+    <div id="treeWrapper" style={{ width: '1000em', height: '200em' }}>
+      <h1>Genre Tree</h1>
+      <Tree
+        data={tree}
+        orientation={"vertical"}
+        rootNodeClassName="node__root"
+        branchNodeClassName="node__branch"
+        leafNodeClassName="node__leaf"
+      />
+    </div>
   )
 }
 export default App;
